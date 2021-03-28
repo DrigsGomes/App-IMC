2ba574962d806ae2fe1e5ecaaf1b5b8a02f380b4
vitorgo04 <rodrigo.vitor169@gmail.com>
2021-03-28 00:40:40 -0300

    Iniciando a estrutura do App!

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+
+export default function App(){
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  return(
+    <View styLe={styles.container}>
+
+      <Text styLe={styles.title}>Calcule seu IMC</Text>
+
+      <TextInput
+      styLe={styles.input} 
+      value={peso}
+      onChangeText={(peso) => setPeso(peso)}
+      placeholder= 'Peso (kg)'
+      keyboardType='numeric'
+      />
+      <TextInput
+      styLe={styles.input} 
+      value={altura}
+      onChangeText={(altura) => setAltura(altura)}
+      placeholder= 'Altura (cm)'
+      keyboardType='numeric'
+
+      />
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  
+  container:{
+    flex:1,
+  },
+
+  title:{
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize:30
+  },
+
+  input:{
+    backgroundColor: '#121212',
+    borderRadius: 10,
+    margin:15,
+    padding: 10,
+    color:'#FFF',
+    fontSize:24,
+
+  }
+
+});
