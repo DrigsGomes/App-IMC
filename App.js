cfdb1f527ebcaee44e823be4a45b3a2b88846159
vitorgo04 <rodrigo.vitor169@gmail.com>
2021-03-29 22:02:55 -0300

    Adicionando funcionalidade ao App.

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
 export default function App(){
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
 
-  return(
-    <View styLe={styles.container}>
+  function handleSubmit(){
+    const alt = altura /100;
+    const imc = peso /(alt * alt);
 
-      <Text styLe={styles.title}>Calcule seu IMC</Text>
+    if (imc < 18.6){
+      alert('Você está abaixo do peso! ' + imc.toFixed(2));
+    }else if (imc >=18.6 && imc < 24.9){
+      alert('Peso ideal! ' + imc.toFixed(2));
+    } else if(imc >= 24.9 && imc < 34.9 ){
+      alert('Levemente acima do peso! ' + imc.toFixed(2));
+
+    }
+
+  
+
+  }
+
+  return(
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Calcule seu IMC</Text>
+      
 
       <TextInput
-      styLe={styles.input} 
+      style={styles.input} 
       value={peso}
       onChangeText={(peso) => setPeso(peso)}
       placeholder= 'Peso (kg)'
@@ -20,13 +38,20 @@ export default function App(){
       />
 
       <TextInput
-      styLe={styles.input} 
+      style={styles.input} 
       value={altura}
       onChangeText={(altura) => setAltura(altura)}
       placeholder= 'Altura (cm)'
       keyboardType='numeric'
 
       />
+
+      <TouchableOpacity 
+      style={styles.button}
+      onPress={handleSubmit}
+      >
+        <Text style={styles.buttonText}> Calcular </Text>
+      </TouchableOpacity>
 
     </View>
 
@@ -54,4 +79,17 @@ const styles = StyleSheet.create({
     fontSize:24,
   },
 
+  button:{
+    justifyContent: 'center',
+    alignItems:'center',
+    margin: 15,
+    backgroundColor: '#41Aef4',
+    padding:10,
+    borderRadius:5
+  }, 
+
+  buttonText:{
+    color:'#FFF',
+    fontSize:25,
+  }
 });
